4557e93b58f9219b674efebc8eabd3922e9c4aff
petaa33 <101650825+petaa33@users.noreply.github.com>
2025-01-22 20:42:16 +0100

    add sponsors fetch

---
+++ _data/silver_sponsors.js
@@ -0,0 +1,10 @@
+import eleventyFetch from "@11ty/eleventy-fetch";
+
+export default async function fetchSponsors(params) {
+	const json = await eleventyFetch("https://opencollective.com/stride3d/tiers/silver-strider/all.json", {
+			duration: "7d",
+			type: "json"
+		});
+		
+	return json;
+}
